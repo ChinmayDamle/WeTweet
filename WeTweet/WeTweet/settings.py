c0efdfaae9bcabb811fa74be75342c78955b0b9e
Chinmay Damle <chinmaydamle96@gmail.com>
2024-09-27 14:40:37 +0530

    commit

--- WeTweet/WeTweet/settings.py
+++ WeTweet/WeTweet/settings.py
@@ -22,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1-pt+z7sxwixk77#1s6pb4kz1!s-ek653uxen$7k+%br_cg7uv"
+# SECRET_KEY = "django-insecure-1-pt+z7sxwixk77#1s6pb4kz1!s-ek653uxen$7k+%br_cg7uv"
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1-pt+z7sxwixk77#1s6pb4kz1!s-ek653uxen$7k+%br_cg7uv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = ['we-tweet.vercel.app', '127.0.0.1']
 
 SITE_ID = 2
 

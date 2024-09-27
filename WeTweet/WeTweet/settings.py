16d9afb0cf389cb3e0c5c8c7c03f1bb88dad96d2
Chinmay Damle <chinmaydamle96@gmail.com>
2024-09-27 14:30:48 +0530

    commit

--- WeTweet/WeTweet/settings.py
+++ WeTweet/WeTweet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1-pt+z7sxwixk77#1s6pb4kz1!s-ek653uxen$7k+%br_cg7uv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 

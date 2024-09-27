24c2ce7ccd6b5e78d45b9b5dd9b5f1262592d100
Chinmay Damle <chinmaydamle96@gmail.com>
2024-09-27 15:27:19 +0530

    commit

--- WeTweet/WeTweet/settings.py
+++ WeTweet/WeTweet/settings.py
@@ -172,8 +172,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# STATIC_URL = "static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_URL = "/accounts/login"
